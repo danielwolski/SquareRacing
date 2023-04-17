03d7b36389c16f56af1f69419f2a51591bff2d39
danielwolski <80836050+danielwolski@users.noreply.github.com>
2023-04-17 21:38:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -16,11 +16,23 @@
 #define TRACK_MARGIN 130
 #define INNER_MARGIN 250
 
+#define NUM_TRACK_POINTS 10
+
 typedef struct {
     Uint8 r;
     Uint8 g;
     Uint8 b;
 } Color;
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+Point track_points[10];
+int directionX = 0, directionY = 0;
+int current_track_point = 0;
+
 
 //kolory sa odwrocone zamiast RGB jest BGR
 const Color CORRIDOR_IN_COLOR = {0, 0, 255};    // red
@@ -89,6 +101,14 @@ bool IsColor(Uint32 pixel, SDL_PixelFormat *format, Color color) {
     return r == color.r && g == color.g && b == color.b;
 }
 
+void DrawPoint(SDL_Renderer* renderer, SDL_Point p) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderDrawPoint(renderer, p.x, p.y);
+    SDL_RenderDrawPoint(renderer, p.x - 1, p.y);
+    SDL_RenderDrawPoint(renderer, p.x, p.y - 1);
+    SDL_RenderDrawPoint(renderer, p.x - 1, p.y - 1);
+}
+
 Uint32 GetPixel(SDL_Surface *surface, int x, int y) {
     int bpp = surface->format->BytesPerPixel;
     Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
@@ -110,6 +130,19 @@ Uint32 GetPixel(SDL_Surface *surface, int x, int y) {
             return 0;
     }
 }
+
+void UpdateDirection(int x, int y) {
+    if (current_track_point + 1 >= sizeof(track_points) / sizeof(track_points[0])) {
+        return;
+    }
+
+    Point next = track_points[current_track_point + 1];
+
+    directionX = next.x > x ? 1 : (next.x < x ? -1 : 0);
+    directionY = next.y > y ? 1 : (next.y < y ? -1 : 0);
+}
+
+
 
 bool CanMove(SDL_Surface *surface, int x, int y, int direction) {
     Uint32 color = 0;
@@ -146,9 +179,16 @@ bool CanMove(SDL_Surface *surface, int x, int y, int direction) {
     return !IsColor(color, surface->format, DONT_ENTER_COLOR);
 }
 
-void UpdateSquarePosition(int *x, int *y, const bool keys[], SDL_Surface *trackSurface) {
+void UpdateSquarePosition(int *x, int *y, const bool keys[], SDL_Surface *trackSurface, int directionX, int directionY) {
     static int speed = PLAYER_SPEED;
     int newX = *x, newY = *y;
+    
+    // Sprawdź, czy gracz dotarł do następnego punktu ścieżki
+    Point next_point = track_points[current_track_point + 1];
+    if (abs(*x - next_point.x) <= PLAYER_SPEED && abs(*y - next_point.y) <= PLAYER_SPEED) {
+        current_track_point++;
+        UpdateDirection(*x,*y);
+    }
 
     // Check whether the speed increase has ended
     if (speed_increase_start != 0 && time(NULL) - speed_increase_start >= NITRO_LAST_TIME) {
@@ -156,15 +196,15 @@ void UpdateSquarePosition(int *x, int *y, const bool keys[], SDL_Surface *trackS
         speed_increase_start = 0;
     }
 
-    if (keys[SDL_SCANCODE_W]) {
+    if (directionY == -1) {
         newY -= speed;
     }
-    if (keys[SDL_SCANCODE_S]) {
+    if (directionY == 1) {
         newY += speed;
     }
     
     // Check whether player un/locks pitstop
-    if (keys[SDL_SCANCODE_W]) {
+    if (directionY == -1) {
         Uint32 colorTop = GetPixel(trackSurface, *x, newY-1);
         printf("Raw colorTop: %u\n", colorTop);
         Uint8 r, g, b;
@@ -189,16 +229,16 @@ void UpdateSquarePosition(int *x, int *y, const bool keys[], SDL_Surface *trackS
 
     newX = *x;
 
-    if (keys[SDL_SCANCODE_A]) {
+    if (directionX == -1) {
         newX -= speed;
     }
-    if (keys[SDL_SCANCODE_D]) {
+    if (directionX == 1) {
         newX += speed;
     }
 
 
     // Check whether player un/locks the corridor
-    if (keys[SDL_SCANCODE_A]) {
+    if (directionX == -1) {
         Uint32 colorTop = GetPixel(trackSurface, newX - 1, *y);
         printf("Raw colorLeft: %u\n", colorTop);
         Uint8 r, g, b;
@@ -223,6 +263,20 @@ void UpdateSquarePosition(int *x, int *y, const bool keys[], SDL_Surface *trackS
 
 int main(int argc, char *argv[]) {
 
+    SDL_Point track_points[NUM_TRACK_POINTS] = {
+        {150, 150},
+        {300, 150},
+        {1000, 150},
+        {1200, 200},
+        {1200, 500},
+        {1000, 540},
+        {510, 540},
+        {75, 485},
+        {75, 210},
+        {135, 180}
+    };
+    
+
     SDL_Surface *trackSurface = NULL;
 
     SDL_Init(SDL_INIT_VIDEO);
@@ -236,8 +290,8 @@ int main(int argc, char *argv[]) {
         // Handle error
     }
 
-    int x = INNER_MARGIN - 10;
-    int y = INNER_MARGIN - 80;
+    int x = 150;
+    int y = 150;
 
     bool keys[SDL_NUM_SCANCODES] = {false};
     bool running = true;
@@ -258,9 +312,6 @@ int main(int argc, char *argv[]) {
     SDL_SetRenderTarget(renderer, NULL);
     SDL_DestroyTexture(target);
 
-
-
-
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -279,7 +330,7 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        UpdateSquarePosition(&x, &y, keys, trackSurface);
+        UpdateSquarePosition(&x, &y, keys, trackSurface, directionX, directionY);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
@@ -287,6 +338,10 @@ int main(int argc, char *argv[]) {
         SDL_RenderCopy(renderer, trackTexture, NULL, NULL);  // Assumes track texture size matches the window size
 
         DrawPlayer(renderer, x, y);
+        
+            for (int i = 0; i < NUM_TRACK_POINTS; i++) {
+            DrawPoint(renderer, track_points[i]);
+            }
         
         DrawLockStatusCircle(renderer, IS_CORRIDOR_LOCKED, -33);
         DrawLockStatusCircle(renderer, IS_PITSTOP_LOCKED, 20);
